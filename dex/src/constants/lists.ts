5bac0b0994d72084bcfd0095089c7694fc0f72d1
we9f29fs <we9f29fs@fwef.com>
2023-03-19 20:31:47 +0800

    给代币json加版本号

--- dex/src/constants/lists.ts
+++ dex/src/constants/lists.ts
@@ -1,5 +1,5 @@
 // 这里存储了默认的代币列表。
-export const DEFAULT_TOKEN_LIST_URL = 'https://swap.tronfund.xyz/token.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://swap.tronfund.xyz/token.json?v=1.1'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
