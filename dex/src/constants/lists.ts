fff5d03e4e3c664ad36c446eb28ba06642110a5e
we9f29fs <we9f29fs@fwef.com>
2023-03-19 13:45:26 +0800

    优化

--- dex/src/constants/lists.ts
+++ dex/src/constants/lists.ts
@@ -1,5 +1,5 @@
 // 这里存储了默认的代币列表。
-export const DEFAULT_TOKEN_LIST_URL = 'https://github.com/tianheng2017/dex/blob/main/dex/token.json'
+export const DEFAULT_TOKEN_LIST_URL = '/token.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
