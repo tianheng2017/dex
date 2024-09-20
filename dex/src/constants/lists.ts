d7585552b188398ad6989cf4968ff7d800b9295b
tianheng2017 <tianheng2017@qq.com>
2024-09-20 13:31:22 +0800

    优化

--- dex/src/constants/lists.ts
+++ dex/src/constants/lists.ts
@@ -1,4 +1,4 @@
 // 这里存储了默认的代币列表。
-export const DEFAULT_TOKEN_LIST_URL = 'http://localhost:3000/token.json'
+export const DEFAULT_TOKEN_LIST_URL = 'http://43.134.228.157/token.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
